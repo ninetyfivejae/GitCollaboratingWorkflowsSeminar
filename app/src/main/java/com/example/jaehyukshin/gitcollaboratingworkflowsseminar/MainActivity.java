3d07b1deea20f29975292c19fd81dd654f4f934c
ninetyfivejae <jh33836782@gmail.com>
2018-07-05 14:54:29 +0900

    Add : messiButton 추가
    누르면 리오넬 메시 소속팀 바르셀로나 토스트 출력

--- app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflowsseminar/MainActivity.java
+++ app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflowsseminar/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.jaehyukshin.gitcollaboratingworkflowsseminar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button messiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messiButton = (Button)findViewById(R.id.messiButton);
+        messiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "FC Barcelona", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

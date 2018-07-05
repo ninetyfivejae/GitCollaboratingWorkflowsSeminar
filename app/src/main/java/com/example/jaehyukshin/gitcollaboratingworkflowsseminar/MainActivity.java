7f9af01e43e31f5adcc10cf8806681b6a94476b9
ninetyfivejae <jh33836782@gmail.com>
2018-07-05 19:37:03 +0900

    Add : Plus 기능 구현

--- app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflowsseminar/MainActivity.java
+++ app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflowsseminar/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.jaehyukshin.gitcollaboratingworkflowsseminar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstNumber;
+    EditText secondNumber;
+    Button plusButton;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstNumber = (EditText)findViewById(R.id.firstNumber);
+        secondNumber = (EditText)findViewById(R.id.secondNumber);
+        result = (TextView)findViewById(R.id.result);
+
+        plusButton = (Button)findViewById(R.id.plusButton);
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int resultNumber = Integer.parseInt(firstNumber.getText().toString()) + Integer.parseInt(secondNumber.getText().toString());
+                result.setText(Integer.toString(resultNumber));
+            }
+        });
     }
 }

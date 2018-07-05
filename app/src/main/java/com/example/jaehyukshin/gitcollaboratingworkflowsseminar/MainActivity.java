54ccbae34705dc45b13e352149d83323a8840606
ninetyfivejae <jh33836782@gmail.com>
2018-07-05 19:42:54 +0900

    Add : multiply 기능 추가

--- app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflowsseminar/MainActivity.java
+++ app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflowsseminar/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.jaehyukshin.gitcollaboratingworkflowsseminar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstNumber;
+    EditText secondNumber;
+    Button plusButton;
+    Button minusButton;
+    Button multiplyButton;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstNumber = (EditText)findViewById(R.id.firstNumber);
+        secondNumber = (EditText)findViewById(R.id.secondNumber);
+        result = (TextView)findViewById(R.id.result);
+
+        plusButton = (Button)findViewById(R.id.plusButton);
+        minusButton = (Button)findViewById(R.id.minusButton);
+        multiplyButton = (Button)findViewById(R.id.multiplyButton);
+
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MultiplyOperation();
+            }
+        });
+    }
+
+    public void MultiplyOperation(){
+        int resultNumber = Integer.parseInt(firstNumber.getText().toString()) * Integer.parseInt(secondNumber.getText().toString());
+        result.setText(Integer.toString(resultNumber));
     }
 }
